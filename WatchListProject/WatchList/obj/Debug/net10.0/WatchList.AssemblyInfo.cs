2132a5cc8e9e0e9af1735b82df1435eb82b8ff93
YongLiang_CS690 <brooklynliang@gmail.com>
2026-02-20 19:01:31 +0000

    Iteration 1

--- WatchListProject/WatchList/obj/Debug/net10.0/WatchList.AssemblyInfo.cs
+++ WatchListProject/WatchList/obj/Debug/net10.0/WatchList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a496e762e578fe9ce353f2956b48dff9a0fd6dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f4679a96245c973b8de2cd58ccbd8908303dfbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
